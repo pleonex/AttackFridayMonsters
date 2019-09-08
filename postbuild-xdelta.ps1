48d52e81a2463fef5a9d10ffcdedea38bf89de69
Benito Palacios Sanchez <benito356@gmail.com>
2019-09-08 13:06:53 +0200

    :bug: Fix patch generation

--- postbuild-xdelta.ps1
+++ postbuild-xdelta.ps1
@@ -1,22 +1,24 @@
 $ErrorActionPreference = "Stop"
 
-Set-Location GameData\full
-
 Write-Host "Generating NCCH"
-& ..\tools\3dstool.exe -ctf cxi .\game.bin `
-    --exefs ..\output\game.3ds.exefs `
-    --romfs ..\output\game.3ds.romfs `
-    --exh .\exheader_ncch0.bin `
-    --plain .\plain.bin `
-    --header .\header_ncch0.bin `
+Remove-Item -ErrorAction Ignore GameData\output\program.bin
+& GameData\tools\3dstool.exe -ctf cxi GameData\output\program.bin `
+    --exefs GameData\output\game.3ds.exefs `
+    --romfs GameData\output\game.3ds.romfs `
+    --exh GameData\rom_metadata\exheader_ncch0.bin `
+    --plain GameData\rom_metadata\plain.bin `
+    --header GameData\rom_metadata\header_ncch0.bin
 
 Write-Host "Generating NCSD"
-& ..\tools\3dstool.exe -ct01f cci `
-    .\game.bin .\manual.bin `
-    modified.3ds `
-    --header .\header_cci.bin
+Remove-Item -ErrorAction Ignore GameData\output\game_patched.3ds
+& GameData\tools\3dstool.exe -ct01f cci `
+    GameData\output\program.bin GameData\rom_metadata\manual.bin `
+    GameData\output\game_patched.3ds `
+    --header GameData\rom_metadata\header_cci.bin
 
 Write-Host "Generating patch"
-& ..\tools\xdelta3.exe -e -s ..\game.3ds modified.3ds ..\output\game.xdelta
-
-Set-Location ..\..\
+Remove-Item -ErrorAction Ignore GameData\output\patch.xdelta
+& GameData\tools\xdelta3.exe -e -S `
+    -s GameData\game.3ds `
+    GameData\output\game_patched.3ds `
+    GameData\output\patch.xdelta
